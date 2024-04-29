af42e81ad26c34b117efa38404443aee151492fe
Sebastian Oviedo <66002824+sebaso-19@users.noreply.github.com>
2024-04-28 20:02:59 -0500

    Add files via upload

---
+++ JPA-Workshop/target/generated-sources/annotations/com/example/models/Competitor_.java
@@ -0,0 +1,25 @@
+package com.example.models;
+
+import java.util.Calendar;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-04-28T19:58:15")
+@StaticMetamodel(Competitor.class)
+public class Competitor_ { 
+
+    public static volatile SingularAttribute<Competitor, String> country;
+    public static volatile SingularAttribute<Competitor, Calendar> createdAt;
+    public static volatile SingularAttribute<Competitor, String> address;
+    public static volatile SingularAttribute<Competitor, Boolean> winner;
+    public static volatile SingularAttribute<Competitor, String> city;
+    public static volatile SingularAttribute<Competitor, String> surname;
+    public static volatile SingularAttribute<Competitor, String> name;
+    public static volatile SingularAttribute<Competitor, String> cellphone;
+    public static volatile SingularAttribute<Competitor, String> telephone;
+    public static volatile SingularAttribute<Competitor, Long> Id;
+    public static volatile SingularAttribute<Competitor, Integer> age;
+    public static volatile SingularAttribute<Competitor, Calendar> updatedAt;
+
+}
